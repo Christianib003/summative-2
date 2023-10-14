6759c9a01dc95f430fe16c62c38481dd2ac2ade1
jmugisha1 <j.mugisha1@alustudent.com>
2023-10-14 12:25:44 +0300

    del

--- main.sh
+++ main.sh
@@ -22,4 +22,18 @@ delete_student() {
     grep -v "$delete_id" students-list_0923.txt > temp.txt
     mv temp.txt students-list_0923.txt
     echo "Student with ID $delete_id deleted."
+
+}
+# Function to update a student record by ID
+update_student() {
+    read -p "Enter student ID to update: " update_id
+
+    read -p "Enter updated student email: " email
+    read -p "Enter updated student age: " age
+
+    awk -v id="$update_id" -v new_email="$email" -v new_age="$age" 'BEGIN { OFS=" " } $3==id { $1=new_email; $2=new_age; } 1' students-list_0923.txt > temp.txt
+
+    mv temp.txt students-list_0923.txt
+
+    echo "Student with ID $update_id updated."
 }
