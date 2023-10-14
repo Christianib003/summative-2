412de8b898dd295046182c1aa42c983f2683b624
Christianib003 <christianib003@gmail.com>
2023-10-14 02:16:02 -0700

    create a function to create a student

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# creatig a student 
+create_student() {
+    read -p "Enter student email: " email
+    read -p "Enter student age: " age
+    read -p "Enter student ID: " student_id
+
+    # Saving the created student  to a file
+    echo "$email $age $student_id" >> students-list_0923.txt
+    echo "Student record created and saved."
+}
